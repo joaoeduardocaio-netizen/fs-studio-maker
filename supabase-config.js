36d6c0ab17261acb3561295474a369726a662fd1
joaoeduardocaio-netizen <joaoeduardocaio@gmail.com>
2026-09-24 14:09:52 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.FS_SUPABASE = Object.freeze({
   url: "https://jdwvulfnscdhvemidrof.supabase.co",
   publishableKey: "sb_publishable_e0b3UIp_U4p8GI4Z-fLVUA_ogaWdS03",
-  adminEmail: "joaoeduardocaio@gmail.com",
+  adminEmail: "fs.studioprinting@gmail.com",
   whatsapp: "393287036017"
 });
